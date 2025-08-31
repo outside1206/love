db927af5d2c87f2a84296640cada0d18d1f08c9a
byun618 <byun618@gmail.com>
2025-08-31 18:09:55 +0900

    코드정리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '@/styles/globals.css'
-import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { NavermapsProvider } from 'react-naver-maps'
@@ -11,14 +10,13 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>My App</title>
         <meta name="description" content="This is my app" />
       </Head>
-      {/* <AnimatePresence> */}
+
       <NavermapsProvider
         ncpKeyId={process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID as string}
       >
         <Component {...pageProps} />
         <div id="global-modal"></div>
       </NavermapsProvider>
-      {/* </AnimatePresence> */}
     </>
   )
 }
