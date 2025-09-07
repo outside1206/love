20cc1030b870f8dc1b21b65e74e05ac5c245422d
byun618 <byun618@gmail.com>
2025-09-08 00:05:59 +0900

    폰트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,20 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>상가 커플 결혼식에 초대하겠습니다.</title>
         <meta name="description" content="come come" />
+        <link
+          href="https://fonts.cdnfonts.com/css/brownhill-script"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.cdnfonts.com/css/anek-bangla"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin=""
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
       </Head>
 
       <NavermapsProvider
