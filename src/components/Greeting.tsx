e82547b70473dd0e9fa37af6ddb7c03aaa29527d
byun618 <byun618@gmail.com>
2025-10-12 20:46:43 +0900

    관계 추가

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -4,6 +4,10 @@ import Image from 'next/image'
 import nextConfig from '../../next.config'
 import SectionTitle from './public/SectionTitle'
 
+interface GreetingProps {
+  visibleRelation?: boolean
+}
+
 const Wrapper = styled(motion.div)`
   padding-bottom: 40px;
 
@@ -19,8 +23,56 @@ const HandCopyWrapper = styled.div`
   margin-top: 40px;
 `
 
-const Greeting = () => {
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  display: block;
+`
+
+const NameWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  font-family: 'Anek Bangla Expanded';
+  font-weight: 400;
+
+  padding: 0 110px;
+`
+
+const Name = styled.div<{ isSmall?: boolean }>`
+  font-size: 17px;
+
+  margin-right: 5px;
+`
+
+const RelationWrapper = styled.div`
+  width: 50px;
+
+  display: flex;
+  gap: 5px;
+`
+
+const Relation = styled.div<{ marginLeft?: number }>`
+  display: flex;
+  align-items: flex-end;
+
+  font-size: 14px;
+  color: #b0b0b0;
+
+  ${({ marginLeft }) => (marginLeft ? `margin-left: ${marginLeft}px` : '')}
+`
+
+const Greeting = ({ visibleRelation }: GreetingProps) => {
   const basePath = nextConfig.basePath ?? ''
+  const galleryNo = 'gallery21'
+  const srcsetWebp = [
+    `${basePath}/images/converted/${galleryNo}-430.webp 430w`,
+    `${basePath}/images/converted/${galleryNo}-860.webp 860w`,
+    `${basePath}/images/converted/${galleryNo}-1290.webp 1290w`,
+    `${basePath}/images/converted/${galleryNo}-1625.webp 1625w`,
+  ].join(', ')
 
   return (
     <Wrapper
@@ -55,7 +107,52 @@ const Greeting = () => {
           width={275}
           height={79}
         />
+        <Image
+          src={`${basePath}/images/raws/hand-copy-4.png`}
+          alt="hand-copy"
+          width={275}
+          height={79}
+        />
+        {visibleRelation && (
+          <picture
+            style={{
+              marginBottom: '20px',
+            }}
+          >
+            <source
+              type="image/webp"
+              srcSet={srcsetWebp}
+              sizes={'(max-width: 430px) calc((100vw - 24px)/3), 350px'}
+            />
+            <Img
+              src={`${basePath}/images/converted/${galleryNo}.jpeg`}
+              alt="intro"
+              loading="lazy"
+              decoding="async"
+            />
+          </picture>
+        )}
       </HandCopyWrapper>
+      {visibleRelation && (
+        <>
+          <NameWrapper>
+            <Name>변정수 · 윤춘화</Name>
+            <RelationWrapper>
+              <Relation>의</Relation>
+              <Relation>아들</Relation>
+            </RelationWrapper>
+            <Name>상현</Name>
+          </NameWrapper>
+          <NameWrapper>
+            <Name>유경석 · 이주희</Name>
+            <RelationWrapper>
+              <Relation>의</Relation>
+              <Relation marginLeft={6}>딸</Relation>
+            </RelationWrapper>
+            <Name>가연</Name>
+          </NameWrapper>
+        </>
+      )}
     </Wrapper>
   )
 }
